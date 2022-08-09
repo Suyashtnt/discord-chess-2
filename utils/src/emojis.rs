09a3ec60fb3dc64c4c10c4ce98ecad98f5b287b3
Suyashtnt <suyashtnt@gmail.com>
2022-08-09 15:18:19 +0200

    fix: fix some infinite recursion

--- utils/src/emojis.rs
+++ utils/src/emojis.rs
@@ -4,42 +4,42 @@ use shakmaty::Piece;
 
 pub enum PieceEmoji {
     // EMPTY SQUARES
-    DarkEmpty = 979399119397355520,
-    LightEmpty = 979399119644799026,
+    DarkEmpty,
+    LightEmpty,
 
     // WHITE PIECES
     // LIGHT
-    LightWhitePawn = 979396455162863676,
-    LightWhiteBishop = 979396454944751656,
-    LightWhiteKnight = 979396455196426280,
-    LightWhiteRook = 979396454986702848,
-    LightWhiteQueen = 979396455112532019,
-    LightWhiteKing = 979396455162871878,
+    LightWhitePawn,
+    LightWhiteBishop,
+    LightWhiteKnight,
+    LightWhiteRook,
+    LightWhiteQueen,
+    LightWhiteKing,
 
     // DARK
-    DarkWhitePawn = 979396455192223824,
-    DarkWhiteBishop = 979396455280304199,
-    DarkWhiteKnight = 979397693493346344,
-    DarkWhiteRook = 979397692914557012,
-    DarkWhiteQueen = 979442406992797716,
-    DarkWhiteKing = 979397693375926272,
+    DarkWhitePawn,
+    DarkWhiteBishop,
+    DarkWhiteKnight,
+    DarkWhiteRook,
+    DarkWhiteQueen,
+    DarkWhiteKing,
 
     // BLACK PIECE
     // LIGHT
-    LightBlackPawn = 979396455255138304,
-    LightBlackBishop = 979396455129313370,
-    LightBlackKnight = 979396455095750686,
-    LightBlackRook = 979396454965731369,
-    LightBlackQueen = 979442406992801822,
-    LightBlackKing = 979396454726647819,
+    LightBlackPawn,
+    LightBlackBishop,
+    LightBlackKnight,
+    LightBlackRook,
+    LightBlackQueen,
+    LightBlackKing,
 
     // DARK
-    DarkBlackPawn = 979396455498416158,
-    DarkBlackBishop = 979396454986711040,
-    DarkBlackKnight = 979397693619187733,
-    DarkBlackRook = 979397693279453255,
-    DarkBlackQueen = 979396454957338634,
-    DarkBlackKing = 979396454886035477,
+    DarkBlackPawn,
+    DarkBlackBishop,
+    DarkBlackKnight,
+    DarkBlackRook,
+    DarkBlackQueen,
+    DarkBlackKing,
 }
 
 impl PieceEmoji {
@@ -156,6 +156,49 @@ impl PieceEmoji {
 
 impl Display for PieceEmoji {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "{}", self)
+        use PieceEmoji::*;
+        write!(
+            f,
+            "{}",
+            match self {
+                // EMPTY SQUARES
+                DarkEmpty => "979399119397355520",
+                LightEmpty => "979399119644799026",
+
+                // WHITE PIECES
+                // LIGHT
+                LightWhitePawn => "979396455162863676",
+                LightWhiteBishop => "979396454944751656",
+                LightWhiteKnight => "979396455196426280",
+                LightWhiteRook => "979396454986702848",
+                LightWhiteQueen => "979396455112532019",
+                LightWhiteKing => "979396455162871878",
+
+                // DARK
+                DarkWhitePawn => "979396455192223824",
+                DarkWhiteBishop => "979396455280304199",
+                DarkWhiteKnight => "979397693493346344",
+                DarkWhiteRook => "979397692914557012",
+                DarkWhiteQueen => "979442406992797716",
+                DarkWhiteKing => "979397693375926272",
+
+                // BLACK PIECE
+                // LIGHT
+                LightBlackPawn => "979396455255138304",
+                LightBlackBishop => "979396455129313370",
+                LightBlackKnight => "979396455095750686",
+                LightBlackRook => "979396454965731369",
+                LightBlackQueen => "979442406992801822",
+                LightBlackKing => "979396454726647819",
+
+                // DARK
+                DarkBlackPawn => "979396455498416158",
+                DarkBlackBishop => "979396454986711040",
+                DarkBlackKnight => "979397693619187733",
+                DarkBlackRook => "979397693279453255",
+                DarkBlackQueen => "979396454957338634",
+                DarkBlackKing => "979396454886035477",
+            }
+        )
     }
 }
